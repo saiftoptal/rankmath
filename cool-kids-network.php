86a816d1baeeb5fb1dd2a65e1ea9cbf384162580
Saif Bin Alam <saif@toptal.com>
2025-01-23 03:44:45 +0000

    plugin initialization

---
+++ cool-kids-network.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Plugin Name: Cool Kids Network
+ * Plugin URI: https://rankmath.saif.london/
+ * Description: This plugin adds the crucial functionalities for the Cool Kids Network
+ * Version: 1.0.0
+ * Author: Saif Bin Alam
+ * Author URI: https://saif.london/
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: rankmath
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Using prefix "rms_" everywhere in the plugin for security and compatibility
+/**
+ * Currently plugin version.
+ */
+define( 'RMS_VERSION', '1.0.0' );
